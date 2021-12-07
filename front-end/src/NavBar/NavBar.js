b14e8b5cf9fbb5d2e5028842be72d290b5569cec
Nick Fuller <24nickfuller24@gmail.com>
2021-12-07 16:51:20 -0600

    some css changes

--- front-end/src/NavBar/NavBar.js
+++ front-end/src/NavBar/NavBar.js
@@ -3,20 +3,31 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components'
 
 const NavWrapper = styled.ul`
+  position: absolute;
+  right: 5rem;
+  width: 600px;
   display: flex;
+  justify-content: space-between;
   list-style-type: none;
 `;
 
 const NavLink = styled.li`
   font-size: 2rem;
   font-weight: 500;
-  color: #003049;
+  color: #F77800;
   font-family: 'Montserrat', sans-serif;
 `;
 
 const StyledLink = styled(Link)`
   text-decoration: none;
-  color: #003049;
+  color: #F77800;
+  font-weight: 1000;
+`;
+
+const LogLink = styled(Link)`
+  text-decoration: none;
+  color: #D62828;
+  font-weight: 1000;
 `;
 
 /* may need some sort of user id for saved and home */
@@ -25,14 +36,14 @@ const NavBar = () => {
     <NavWrapper>
       <NavLink><StyledLink to="/post">Post</StyledLink></NavLink>
       <NavLink><StyledLink to="/saved">Saved Material</StyledLink></NavLink>
-      <NavLink><StyledLink to="/" onClick={logout}>Logout</StyledLink></NavLink>
+      <NavLink><LogLink to="/" onClick={logout}>Logout</LogLink></NavLink>
     </NavWrapper>
   );
 };
 
 /* add logout functionality */
 const logout = () => {
-  alert("Are you sure you want to logout");
+  window.confirm("Are you sure you want to logout");
 };
 
 export default NavBar;
