0d303aac3e69fb2fe1a4dfc8db1761ac2d6098ee
Nick Fuller <24nickfuller24@gmail.com>
2021-12-07 16:05:54 -0600

    PostForm page layout

--- front-end/src/PostForm/PostForm.js
+++ front-end/src/PostForm/PostForm.js
@@ -1,5 +1,6 @@
 import NavBar from '../NavBar/NavBar';
 import Header from '../Header/Header';
+import './PostForm.css';
 
 function PostForm() {
   return (
@@ -7,6 +8,38 @@ function PostForm() {
       <Header/>
       <NavBar/>
       <h1>Post a Test</h1>
+      <form>
+        <div id="leftColumn">
+          <label for="school">School</label>
+          <input type="text" id="school" required/>
+          <label for="professor">Professor</label>
+          <input type="text" id="professor" required/>
+          <label for="classCode">Class Code</label>
+          <input type="text" id="classCode" required/>
+          <label for="dept">Department</label>
+          <input type="text" id="dept" required/>
+          <label for="semester">Semester</label>
+          <input type="text" id="semester" required/>
+          <label for="year">Year</label>
+          <input type="text" id="year" required/>
+        </div>
+        <div id="rightColumn">
+          <p>Pick what <b>best</b> describes this file:</p>
+          <div id="typeForm">
+            <input type="radio" id="test" name="type" value="Test"/>
+            <label for="test" class="radio">Test</label><br/>
+            <input type="radio" id="study" name="type" value="Study Guide"/>
+            <label for="study" class="radio">Study Guide</label><br/>
+            <input type="radio" id="homework" name="type" value="Homework"/>
+            <label for="homework" class="radio">Homework</label>
+          </div>
+          <label for="grade">Grade</label>
+          <input type="text" id="grade" required/>
+          <label for="fileUpload">Upload a File</label>
+          <input type="file" id="fileUpload"required/>
+        </div>
+        <input type="submit"/>
+      </form>
     </div>
   );
 }
