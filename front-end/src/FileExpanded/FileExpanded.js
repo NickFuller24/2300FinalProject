4e1e076715c9184bdb12b5dcf189bdf29c3a373c
Nick Fuller <24nickfuller24@gmail.com>
2021-12-07 21:23:50 -0600

    File expanding page

--- front-end/src/FileExpanded/FileExpanded.js
+++ front-end/src/FileExpanded/FileExpanded.js
@@ -4,30 +4,38 @@ import * as data from '../HomePage/test.json';
 import { useParams } from 'react-router-dom';
 
 const CardWrapper = styled.div`
-  width: 400px;
-  height: 600px;
+  width: 100vw;
+  height: 100vh;
   margin: 6rem 5rem 0 5rem;
+  display: grid;
+  grid-template-columns: 800px 1rem auto;
 `;
 
 const Title = styled.h3`
-  font-size: 1.5rem;
+  font-size: 2rem;
   font-weight: 700;
   display: inline;
   margin-right: 0.5rem;
 `;
 
-const Category = styled.div`
+const TextWrapper = styled.div`
+  grid-column-start: 3;
 `;
 
 const PDFWrapper = styled.div`
-  width: 400px;
-  height: 400px;
+  width: 800px;
+  height: 800px;
   background-color: blue;
+  display: inline-block;
 `;
 
 const Description = styled.p`
-  font-size: 1.5rem;
+  font-size: 2rem;
   display: inline;
+`;
+
+const Category = styled.div`
+  margin-bottom: 1.5rem;
 `;
 
 const FileExpanded = (props) => {
@@ -40,18 +48,32 @@ const FileExpanded = (props) => {
         <div className="FileExpanded">
           <CardWrapper>
             <PDFWrapper/>
-            <Category>
-              <Title>Class:</Title>
-              <Description>{data.files[file].class}</Description>
-            </Category>
-            <Category>
-              <Title>Title:</Title>
-              <Description>{data.files[file].title}</Description>
-            </Category>
-            <Category>
-              <Title>Semester:</Title>
-              <Description>{data.files[file].semester}</Description>
-            </Category>
+            <TextWrapper>
+              <Category>
+                <Title>Class:</Title>
+                <Description>{data.files[file].class}</Description>
+              </Category>
+              <Category>
+                <Title>Title:</Title>
+                <Description>{data.files[file].title}</Description>
+              </Category>
+              <Category>
+                <Title>Semester:</Title>
+                <Description>{data.files[file].semester}</Description>
+              </Category>
+              <Category>
+                <Title>Professor:</Title>
+                <Description>{data.files[file].professor}</Description>
+              </Category>
+              <Category>
+                <Title>School:</Title>
+                <Description>{data.files[file].school}</Description>
+              </Category>
+              <Category>
+                <Title>Grade:</Title>
+                <Description>{data.files[file].grade}</Description>
+              </Category>
+            </TextWrapper>
           </CardWrapper>
         </div>
       );
