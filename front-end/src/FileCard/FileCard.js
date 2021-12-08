147fff556c830bb14ea24bac5386268b01effaa5
Nick Fuller <24nickfuller24@gmail.com>
2021-12-07 19:47:27 -0600

    loading files from json

--- front-end/src/FileCard/FileCard.js
+++ front-end/src/FileCard/FileCard.js
@@ -1,10 +1,51 @@
 import React from 'react';
 import styled from 'styled-components'
 
+const CardWrapper = styled.div`
+  width: 400px;
+  height: 600px;
+  margin-right: 1rem;
+`;
 
+const Title = styled.h3`
+  font-size: 1.5rem;
+  font-weight: 700;
+  display: inline;
+  margin-right: 0.5rem;
+`;
 
-const FileCard = () => {
+const Category = styled.div`
+`;
 
+const PDFWrapper = styled.div`
+  width: 400px;
+  height: 400px;
+  background-color: blue;
+`;
+
+const Description = styled.p`
+  font-size: 1.5rem;
+  display: inline;
+`;
+
+const FileCard = (props) => {
+  return (
+    <CardWrapper>
+      <PDFWrapper/>
+      <Category>
+        <Title>Class:</Title>
+        <Description>{props.class}</Description>
+      </Category>
+      <Category>
+        <Title>Title:</Title>
+        <Description>{props.title}</Description>
+      </Category>
+      <Category>
+        <Title>Semester:</Title>
+        <Description>{props.semester}</Description>
+      </Category>
+    </CardWrapper>
+  );
 };
 
 export default FileCard;
--- front-end/src/HomePage/HomePage.js
+++ front-end/src/HomePage/HomePage.js
@@ -1,12 +1,37 @@
 import NavBar from '../NavBar/NavBar';
 import Header from '../Header/Header';
+import FileCard from '../FileCard/FileCard';
+import './HomePage.css';
+import * as data from './test.json';
+
 
 function HomePage() {
+  var fileInfo = [];
+  for(const file in data.files) {
+    fileInfo.push(data.files[file]);
+  }
+  // console.log(fileInfo);
   return (
     <div className="HomePage">
       <Header/>
       <NavBar/>
-      <h1>This will be the list of all files</h1>
+      <div id="contentWrapper">
+        <div id="filter"></div>
+        <div id="files">
+          {/* <FileCard title="This is a super long file name to test what the width is like"
+                    semester="Spring 2022"
+                    class="Calc 2"/>
+          <FileCard title="This is a super long file name to test what the width is like"
+                    semester="Spring 2022"
+                    class="Calc 2"/>
+          <FileCard title="This is a super long file name to test what the width is like"
+                    semester="Spring 2022"
+                    class="Calc 2"/> */}
+          {fileInfo.map((fileInfo) => {
+            return <FileCard key={fileInfo.title} class={fileInfo.class} title={fileInfo.title} semester={fileInfo.semester}/>
+          })}
+        </div>
+      </div>
     </div>
   );
 }
